890f8a99f27aff43d25b2e7f9672a9c428464418
Alireza Hosseinkhani <alireza.8096@chmail.ir>
2021-01-29 15:02:32 +0330

    create models and migrate

--- Yektanet-django/yektanet/advertiser_management/models.py
+++ Yektanet-django/yektanet/advertiser_management/models.py
@@ -1,3 +1,22 @@
+from abc import ABC, abstractmethod
+
 from django.db import models
 
-# Create your models here.
+
+class BaseAdvertising(models.Model):
+    clicks = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+
+    class Meta:
+        abstract = True
+
+
+class Advertiser(BaseAdvertising):
+    name = models.CharField(max_length=30)
+
+
+class Ad(BaseAdvertising):
+    title = models.CharField(max_length=30)
+    image_url = models.CharField(max_length=100)
+    link = models.CharField(max_length=100)
+    advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
