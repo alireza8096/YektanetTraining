74868c2489c11ab1cfe0e99a8d815fcc64b89f21
Alireza Hosseinkhani <alireza.8096@chmail.ir>
2021-01-30 00:42:09 +0330

    handle input validation and improve view codes

--- Yektanet-django/yektanet/advertiser_management/views.py
+++ Yektanet-django/yektanet/advertiser_management/views.py
@@ -8,17 +8,12 @@ from django.template import loader
 def ads_index(request):
     advertisers = Advertiser.objects.all()
     inc_list_views(advertisers)
-    template = loader.get_template('advertiser_management/ads.html')
-    context = {
-        'advertisers': advertisers
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'advertiser_management/ads.html', {'advertisers': advertisers})
 
 
 def click_ad(request, ad_id):
     ad = Ad.objects.get(pk=ad_id)
     ad.inc_clicks()
-    print(ad.clicks)
     return redirect(ad.link)
 
 
@@ -33,11 +28,17 @@ def create_ad(request):
 
 
 def submit(request):
-    # todo : Error handling
     advertiser_id = request.POST['advertiser_id']
+    try:
+        advertiser = Advertiser.objects.get(pk=advertiser_id)
+    except():
+        return render(request, 'advertiser_management/create_ad_form.html', {'error_message': 'Error! Invalid ID!'})
     image_url = request.POST['image_url']
     link = request.POST['link']
     title = request.POST['title']
+    if image_url is None or link is None or title is None:
+        return render(request, 'advertiser_management/create_ad_form.html', {'error_message': 'Error! Blank Input Is '
+                                                                                              'Invalid!'})
     ad = Ad(title=title, image_url=image_url, link=link, advertiser_id=advertiser_id)
     ad.save()
     return redirect("http://127.0.0.1:8000/ads/")
