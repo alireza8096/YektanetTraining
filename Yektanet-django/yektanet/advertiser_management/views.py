05311793b1cac02dd21997cdfe260e58d60190dd
Alireza Hosseinkhani <alireza.8096@chmail.ir>
2021-01-29 20:10:10 +0330

    add click counter and redirect for ads

--- Yektanet-django/yektanet/advertiser_management/views.py
+++ Yektanet-django/yektanet/advertiser_management/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Advertiser
 from .models import Ad
 from django.template import loader
@@ -7,11 +7,22 @@ from django.template import loader
 
 def ads_index(request):
     advertisers = Advertiser.objects.all()
-    for advertiser in advertisers:
-        for ad in advertiser.ad_set.all():
-            ad.inc_views()
+    inc_list_views(advertisers)
     template = loader.get_template('advertiser_management/ads.html')
     context = {
         'advertisers': advertisers
     }
     return HttpResponse(template.render(context, request))
+
+
+def click_ad(request, ad_id):
+    ad = Ad.objects.get(pk=ad_id)
+    ad.inc_clicks()
+    print(ad.clicks)
+    return redirect(ad.link)
+
+
+def inc_list_views(advertisers):
+    for advertiser in advertisers:
+        for ad in advertiser.ad_set.all():
+            ad.inc_views()
--- Yektanet-django/yektanet/yektanet/urls.py
+++ Yektanet-django/yektanet/yektanet/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ads/', include('advertiser_management.urls')),
+    path('click/', include('advertiser_management.urls')),
 ]
