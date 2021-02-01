d151d28869052691afbf064d20ec62057c28f721
Alireza Hosseinkhani <alireza.8096@chmail.ir>
2021-02-01 23:18:37 +0330

    create middleware for getting user's ip

---
+++ Yektanet-django/yektanet/advertiser_management/middleware.py
@@ -0,0 +1,13 @@
+class IpMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        request.session['user_ip'] = ip
+        return self.get_response(request)
--- Yektanet-django/yektanet/yektanet/settings.py
+++ Yektanet-django/yektanet/yektanet/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'advertiser_management.middleware.IpMiddleware',
 ]
 
 ROOT_URLCONF = 'yektanet.urls'
