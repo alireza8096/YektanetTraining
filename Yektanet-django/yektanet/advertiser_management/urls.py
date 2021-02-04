a21a543fe019ad209568126f560d01c9337314d8
Alireza Hosseinkhani <alireza.8096@chmail.ir>
2021-02-04 23:22:38 +0330

    complete part six

--- Yektanet-django/yektanet/advertiser_management/urls.py
+++ Yektanet-django/yektanet/advertiser_management/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<int:ad_id>/', views.AdClickView.as_view(), name='click-ad'),
     path('create_ad/', views.CreateAdView.as_view(), name='create-ad'),
     path('submit/', views.SubmitAdView.as_view(), name='submit-ad'),
+    path('stats/', views.StatsView.as_view(), name='stats-view')
 ]
--- Yektanet-django/yektanet/advertiser_management/views.py
+++ Yektanet-django/yektanet/advertiser_management/views.py
@@ -1,11 +1,29 @@
+from django.db.models import Count
+from django.db.models.functions import TruncHour
+from django.forms import models
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
+from django.views.generic import TemplateView
 
 from .models import Advertiser
-from .models import Ad
+from .models import Ad, Click
+from .models import View as ModelView
 from django.template import loader
 from django.utils import timezone
+
+
+class StatsView(TemplateView):
+    template_name = 'advertiser_management/stats.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['click_sum'] = list(get_number_of_clicks_or_views_per_hour_with_id(Click))
+        context['view_sum'] = list(get_number_of_clicks_or_views_per_hour_with_id(ModelView))
+        context['total_click_per_view'] = Click.objects.count() / ModelView.objects.count()
+        context['average_delta_click_and_view'] = get_average_delta_click_and_time()
+        context['click_per_view_rate_per_hour'] = get_click_per_view_rate_per_hour()
+        return context
 
 
 class AdsIndexView(View):
@@ -20,7 +38,7 @@ class AdsIndexView(View):
         for advertiser in advertisers:
             for ad in advertiser.ad_set.all():
                 ad.view_set.create(ip=user_ip, time=time)
-                print(ad.view_set.all())
+                # print(ad.view_set.all())
 
 
 class AdClickView(View):
@@ -53,3 +71,49 @@ class SubmitAdView(View):
         ad = Ad(title=title, image_url=image_url, link=link, advertiser_id=advertiser_id)
         ad.save()
         return redirect("/ads/")
+
+
+def get_number_of_clicks_or_views_per_hour_with_id(model):
+    return model.objects.annotate(hour_and_date=TruncHour('time')).values('ad_id', 'hour_and_date') \
+        .annotate(repeat=Count('ad_id')).values('ad_id', 'hour_and_date', 'repeat')
+
+
+def get_delta_view_and_click(click_id):
+    click = Click.objects.get(pk=click_id)
+    view = ModelView.objects.filter(ip=click.ip, ad_id=click.ad_id, time__lt=click.time).order_by('-time')[0]
+    return click.time - view.time
+
+
+def get_average_delta_click_and_time():
+    clicks_num = Click.objects.count()
+    delta = timezone.now()
+    for click in Click.objects.all():
+        delta += get_delta_view_and_click(click.id)
+    delta -= timezone.now()
+    print(delta / clicks_num)
+    get_click_per_view_rate_per_hour()
+    return delta / clicks_num
+
+
+def get_number_of_clicks_or_views_per_hour_without_id(model):
+    return model.objects.annotate(hour_and_date=TruncHour('time')).values('hour_and_date') \
+        .annotate(repeat=Count('hour_and_date')).values('hour_and_date', 'repeat').order_by('hour_and_date')
+
+
+def get_list_of_hours_for_model(model):
+    return model.objects.annotate(hour_and_date=TruncHour('time')).values('hour_and_date') \
+        .annotate(repeat=Count('hour_and_date')).values('hour_and_date').order_by('hour_and_date')
+
+
+def get_click_per_view_rate_per_hour():
+    clicks_list = list(get_number_of_clicks_or_views_per_hour_without_id(Click))
+    view_list = list(get_number_of_clicks_or_views_per_hour_without_id(ModelView))
+    clicks_hour_list = list(get_list_of_hours_for_model(Click))
+    view_hour_list = list(get_list_of_hours_for_model(ModelView))
+    final_list = []
+    for time in view_hour_list:
+        if time in clicks_hour_list:
+            rate = clicks_list[clicks_hour_list.index(time)]['repeat'] / view_list[view_hour_list.index(time)]['repeat']
+            final_list.append({'date': time['hour_and_date'], 'rate': rate})
+    print(final_list)
+    return final_list
